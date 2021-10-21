a2b95cec51cd35c5ef15c6539dbcfe031cf10d8f
zhou-l <zhou.liang.86.mail@gmail.com>
2021-10-21 11:30:10 +0800

    Replaced body part paths with corrected ones.

---
+++ data/imagePaths/convertImageToPath.bat
@@ -0,0 +1,24 @@
+﻿imageoutline .\male\perineum.png -o plain -i 1 > .\male\pathPerineum.txt
+imageoutline .\male\torso.png -o plain -i 1 > .\male\pathTorso.txt
+imageoutline .\male\head.png -o plain -i 1 > .\male\pathHeadNeck.txt
+imageoutline .\male\Extremities2.png -o plain -i 1 > .\male\pathExtremities2.txt
+imageoutline .\male\PalmSole3.png -o plain -i 1 > .\male\pathPalmSole3.txt
+imageoutline .\male\PalmSole2.png -o plain -i 1 > .\male\pathPalmSole2.txt
+imageoutline .\male\Extremities3.png -o plain -i 1 > .\male\pathExtremities3.txt
+imageoutline .\male\Extremities1.png -o plain -i 1 > .\male\pathExtremities1.txt
+imageoutline .\male\PalmSole4.png -o plain -i 1 > .\male\pathPalmSole4.txt
+imageoutline .\male\PalmSole1.png -o plain -i 1 > .\male\pathPalmSole1.txt
+imageoutline .\male\Extremities4.png -o plain -i 1 > .\male\pathExtremities4.txt
+
+imageoutline .\female\perineum.png -o plain -i 1 > .\female\pathPerineum.txt
+imageoutline .\female\torso.png -o plain -i 1 > .\female\pathTorso.txt
+imageoutline .\female\head.png -o plain -i 1 > .\female\pathHeadNeck.txt
+imageoutline .\female\Extremities2.png -o plain -i 1 > .\female\pathExtremities2.txt
+imageoutline .\female\PalmSole3.png -o plain -i 1 > .\female\pathPalmSole3.txt
+imageoutline .\female\PalmSole2.png -o plain -i 1 > .\female\pathPalmSole2.txt
+imageoutline .\female\Extremities3.png -o plain -i 1 > .\female\pathExtremities3.txt
+imageoutline .\female\Extremities1.png -o plain -i 1 > .\female\pathExtremities1.txt
+imageoutline .\female\PalmSole4.png -o plain -i 1 > .\female\pathPalmSole4.txt
+imageoutline .\female\PalmSole1.png -o plain -i 1 > .\female\pathPalmSole1.txt
+imageoutline .\female\Extremities4.png -o plain -i 1 > .\female\pathExtremities4.txt
+
